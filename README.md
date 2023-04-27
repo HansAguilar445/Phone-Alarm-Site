d29138d4a5f3e10522fb88831134470e25fdfac6
Hans Aguilar <hans.christian.aguilar@gmail.com>
2023-04-26 19:40:19 -0500

    Add README

---
+++ README.md
@@ -0,0 +1,8 @@
+# [Access the project here!](https://hansaguilar445.github.io/Phone-Alarm-Site/)
+## Phone Alarm Site
+This was something I made for the Object Oriented JavaScript course in 2022. We were tasked by our teacher to create a functional 
+alarm clock, complete with a custom sound to play when the set time arrives and an updating time visual. To this end, I modeled mine
+after my iPhone and the iOS Clock app, displaying the time and the inputs on it while laying it down on a table. Once the user inputs
+a time, it displays the set time under the time like the Clock app does with a timer. Once the set time is reached, the alarm 
+goes off and plays a sound. When typing a time, it must be in the 24-hour format and the colon is autofilled. Any invalid inputs are
+ignored.
--- assets/js/alarm.js
+++ assets/js/alarm.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let timeFilter = /[01][0-9]:[0-5][0-9]/;
 const time = document.querySelector('h2');
 const alarmTime = document.querySelector('p');
 const timeInput = document.forms['alarm']['time'];
@@ -14,17 +15,30 @@ const alarm = new Audio('assets/media/police.mp3');
 alarm.type = 'audio/mp3';
 
 document.addEventListener('keypress', function(event) {
-	if (timeInput.value.trim().length === 2 && event.key != ':') {
+	if (timeInput.value.trim().length === 2 && event.key != ':')
 		timeInput.value += ':';
+	
+});
+
+document.addEventListener('keyup', function(event) {
+	if (timeInput.value.length > 5)
+			timeInput.value = timeInput.value.substring(0, 5);
+	else {
+		if (timeInput.value.length === 1 && timeInput.value[0] === 2)
+			timeFilter = /2[0-3]:[0-5][0-9]/;
+		else timeFilter = /[01][0-9]:[0-5][0-9]/;
 	}
 });
 
 setAlarm.addEventListener('click', function() {
 	const timeCheck = timeInput.value.split(':');
-	if (timeInput.value.trim().length === 5 && !isNaN(timeCheck[0]) && !isNaN(timeCheck[1])) {
+	
+	if (timeInput.value.match(timeFilter)) {
 		aTime.setHours(timeCheck[0], timeCheck[1], 0);
 		alarmTime.innerHTML = `<i class="fa-solid fa-bell"></i> ${timeCheck[0]}:${timeCheck[1]}`
 	}
+
+	timeInput.value = '';
 });
 
 setInterval(() => {
